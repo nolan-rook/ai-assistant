6cbcb867d4e330409d055bfa722049b2aae265d0
nolan-rook <nolan@baise.nl>
2024-02-09 11:43:45 +0100

    handling processing your request moved to Voiceflow

--- src/app.py
+++ src/app.py
@@ -94,17 +94,6 @@ def handle_dm_events(event, say):
 
         # Create a unique conversation ID using user_id and thread_ts
         conversation_id = f"{user_id}-{thread_ts}"
-
-        processing_messages = [
-            "On it!", "Sure thing!", "Got it!", "One moment...", "I'm on it!", "Absolutely!",
-            "Understood!", "Just a moment...", "Right away!", "Affirmative!", "No problem!",
-            "Okay!", "On my way!", "Certainly!", "Acknowledged!", "Will do!", "You got it!",
-            "I'm at it!", "Working on it!", "Already on it!", "On top of it!", "I've got this!",
-            "Taking care of it!", "All over it!"
-        ]
-
-        # Send a random processing message
-        say(text=random.choice(processing_messages), thread_ts=thread_ts)
             
         combined_input = user_input
 
@@ -156,17 +145,6 @@ def handle_app_mention_events(event, say):
 
     # Create a unique conversation ID using user_id and thread_ts
     conversation_id = f"{user_id}-{thread_ts}"
-    
-    processing_messages = [
-        "On it!", "Sure thing!", "Got it!", "One moment...", "I'm on it!", "Absolutely!",
-        "Understood!", "Just a moment...", "Right away!", "Affirmative!", "No problem!",
-        "Okay!", "On my way!", "Certainly!", "Acknowledged!", "Will do!", "You got it!",
-        "I'm at it!", "Working on it!", "Already on it!", "On top of it!", "I've got this!",
-        "Taking care of it!", "All over it!"
-    ]
-
-    # Send a random processing message
-    say(text=random.choice(processing_messages), thread_ts=thread_ts)
 
     combined_input = user_input
 
